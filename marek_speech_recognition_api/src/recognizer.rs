e7b492166def34e6260e63f083d361ebaa388334
Marek G <marek-dev@yandex.com>
2023-04-04 12:41:32 +0200

    Update documentation

--- marek_speech_recognition_api/src/recognizer.rs
+++ marek_speech_recognition_api/src/recognizer.rs
@@ -11,8 +11,13 @@ pub trait Recognizer {
     async fn start(&mut self) -> SpeechResult;
 
     /// Process new chunk of data.
+    ///
     /// It waits the time needed to process the data,
     /// so the stop method can always be fast.
+    ///
+    /// For 'is_realtime_only' mode the different behaviour is better.
+    /// The first call is quick, but the subsequent calls should wait
+    /// for the right (real) time.
     async fn write(&mut self, buffer: &[i16]) -> SpeechResult;
 
     /// Stops the recognition.
